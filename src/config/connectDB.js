c08353e041465a347e011dd83c9d1ecac88589e0
drkhaikDev <trinhkhaiviet@gmail.com>
2024-02-21 14:59:49 +0700

    test render 2

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 
 const connectToDatabase = () => {
     return new Promise((resolve, reject) => {
-        mongoose.connect(process.env.DATABASE_URI);
+        let databaseURI = process.env.DATABASE_URI;
+        if (process.env.NODE_ENV === 'production') {
+            // Use production database URI
+            databaseURI = process.env.PRODUCTION_DATABASE_URI;
+        }
+        mongoose.connect(databaseURI);
 
         mongoose.connection.on("error", (err) => {
             console.log("Connect to DB fail", err.message);
