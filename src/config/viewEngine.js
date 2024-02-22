d668d27f9bc9b7bb98c4d5a60029a2c47fb7d11f
drkhaikDev <trinhkhaiviet@gmail.com>
2024-02-22 20:32:57 +0700

    Remove src from repository

--- src/config/viewEngine.js
+++
@@ -1,9 +0,0 @@
-import Express from "express";
-
-let configViewEngine = (app) => {
-    app.use(Express.static("./src/public"));
-    app.set("view engine", "ejs"); // jsp blade
-    app.set("views", "./src/views");
-}
-
-module.exports = configViewEngine;
